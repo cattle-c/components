e6a96fb29b96254b76f70fd98c320a7769678ca8
cattle_chen <cattle_chen@outlook.com>
2022-11-23 19:08:31 +0800

    更新icon

---
+++ packages/vue2/icon/src/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div :class="['c-icon', {
+    [`c-icon${name}`] : name.length
+  }]"></div>
+</template>
+<script>
+export default {
+  props: {
+    name: {
+      type: String
+    }
+  }
+}
+</script>
